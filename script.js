9939e17af839259545ba7ae274822096628e4e7f
PranayTyagi1 <>
2021-05-08 06:21:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('button').addEventListener('click', function () {
+  const day = document.querySelector('.day').value
+  const age = document.querySelector('.age').value
+  if ((day === 'thursday' || day === 'tuesday') || (age > 12 && age < 21)) {
+    alert('You get a discount')
+  } else {
+    alert('You do not get a discount')
+  }
+})
